afd46d60a04df4406c7eb63590bf62303f3f4e94
JDXisCool <JDXGmaing@gmail.com>
2022-08-04 20:32:22 +0530

    Initial commit

---
+++ Debugging with laurel/Challange - 7.js
@@ -0,0 +1,36 @@
+// hi
+moveForward();
+moveForward();
+collect();
+collect();
+turnLeft();
+moveForward();
+turnLeft();
+moveForward();
+for (var count = 0; count < 2; count++) {
+  collect();
+}
+for (var count2 = 0; count2 < 2; count2++) {
+  moveForward();
+}
+turnLeft();
+for (var count3 = 0; count3 < 2; count3++) {
+  moveForward();
+}
+for (var count4 = 0; count4 < 2; count4++) {
+  collect();
+}
+turnLeft();
+moveForward();
+turnRight();
+moveForward();
+collect();
+collect();
+
+/*
+collect();
+*/
+
+/*
+turnRight();
+*/
